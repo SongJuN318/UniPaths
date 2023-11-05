1bad53c86f4e329c671aee354c8f3a9401db314c
Chin Zheng Yu <22004883@siswa.um.edu.my>
2023-11-05 22:04:49 +0800

    personality test main - zhengyu

---
+++ app/src/main/java/com/example/unipaths/Activities/Activity_personality_main.java
@@ -0,0 +1,16 @@
+package com.example.unipaths.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.unipaths.R;
+
+public class Activity_personality_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality_main);
+    }
+}

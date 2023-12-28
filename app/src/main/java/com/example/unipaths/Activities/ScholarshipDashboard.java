555123c29d66f593f733825917c7c910e7df22cf
egsee <egsee@gmail.com>
2023-12-28 16:48:16 +0800

    added dashboard

--- app/src/main/java/com/example/unipaths/Activities/ScholarshipDashboard.java
+++ app/src/main/java/com/example/unipaths/Activities/ScholarshipDashboard.java
@@ -1,6 +1,51 @@
 package com.example.unipaths.Activities;
 
-public class ScholarshipDashboard {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.unipaths.R;
+
+public class ScholarshipDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scholarship_dashboard);
+
+        Button listButton = findViewById(R.id.ListButton);
+        Button tipsButton = findViewById(R.id.TipsButton);
+        Button notiButton = findViewById(R.id.NotiButton);
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start ScholarshipMainpage activity
+                Intent intent = new Intent(ScholarshipDashboard.this, ScholarshipMainPage.class);
+                startActivity(intent);
+            }
+        });
+
+        tipsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start ScholarshipInterviewtips activity
+                Intent intent = new Intent(ScholarshipDashboard.this, ScholarshipInterviewTips.class);
+                startActivity(intent);
+            }
+        });
+
+        notiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start scholarshipnotifications activity
+                Intent intent = new Intent(ScholarshipDashboard.this, ScholarshipNotification.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
